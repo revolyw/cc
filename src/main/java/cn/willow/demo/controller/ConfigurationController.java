d6551484532e5d30b9b7792209dc1299895609f7
Willow <revolyw@yeah.net>
2017-05-23 11:03:20 +0800

    change getting properties interface

--- src/main/java/cn/willow/demo/controller/ConfigurationController.java
+++ src/main/java/cn/willow/demo/controller/ConfigurationController.java
@@ -1,6 +1,7 @@
 package cn.willow.demo.controller;
 
 import cn.willow.demo.framework.Cgi;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,19 +15,32 @@ import java.util.Properties;
 @RestController
 @RequestMapping("/")
 public class ConfigurationController {
-    @RequestMapping("/jdbc/javavirtual")
-    public String getJavaVirtaulDbConfiguration(Cgi cgi) throws IOException {
+    private static final String PREFIX = "/var/www/cc";
+
+    @RequestMapping("/jdbc")
+    public String getPropertiesByProject(Cgi cgi) throws IOException {
         String project = cgi.getString("project", "");
-        String key = cgi.getString("key", "");
+        String cipher = cgi.getString("cipher", "");
         Properties properties = new Properties();
-        FileInputStream inputStream = new FileInputStream("/Users/" + project + ".properties");
+        FileInputStream inputStream = new FileInputStream(PREFIX + "/" + project + ".properties");
         properties.load(inputStream);
-        String cipher = properties.getProperty("cipher");
-        String jdbc = properties.getProperty("jdbc_url");
+        if (properties.isEmpty()) {
+            return "get properties error";
+        }
+        String savedCipher = properties.getProperty("cipher");
+        if (cipher.isEmpty() || savedCipher.isEmpty() || !cipher.equals(savedCipher)) {
+            return "authentication is failed";
+        }
+        String jdbc_url = properties.getProperty("jdbc_url");
         String jdbc_user = properties.getProperty("jdbc_user");
         String jdbc_password = properties.getProperty("jdbc_password");
-        System.out.println(cipher + "," + jdbc + "," + jdbc_user + "," + jdbc_password);
+        JSONObject propertiesJson = new JSONObject();
+        propertiesJson.put("jdbc_url", jdbc_url);
+        propertiesJson.put("jdbc_user", jdbc_user);
+        propertiesJson.put("jdbc_password", jdbc_password);
+        System.out.println(propertiesJson.toString());
         inputStream.close();
-        return key;
+
+        return propertiesJson.toString();
     }
 }
